6ac4cf4759f61d257b1fa672451560c80cb74af2
gamedaybyday <gamedaybyday@30016ba7-4c49-4331-af59-b1367cd06790>
2016-03-15 10:27:26 +0000

    *修改模板文件名 *测颜值http

---
+++ EgretProjects/xb.face/src/com/event/MyEvent.ts
@@ -0,0 +1,10 @@
+/**
+ * 自定义事件
+ * @author 
+ *
+ */
+class MyEvent extends egret.Event{
+	public constructor(type:string, data) {
+    	super(type,false,false,data);
+	}
+}
---
+++ EgretProjects/xb.face/src/com/view/MyTeamScene.ts
@@ -0,0 +1,24 @@
+/**
+ * 我的团队页面
+ * @author 
+ *
+ */
+class MyTeamScene extends BaseScene{
+    private backBtn:eui.Image;   //返回按钮
+    
+	public constructor() {
+    	super("MyTeamSceneSkin");
+	}
+	
+    public componentCreated(): void {
+        super.componentCreated();
+    }
+	
+    public onEnable(): void {
+        
+    }
+
+    public onRemove(): void {
+
+    }
+}
