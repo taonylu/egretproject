2400aaf01143706c61968bb0dc05d240c215624d
gamedaybyday <gamedaybyday@30016ba7-4c49-4331-af59-b1367cd06790>
2017-01-06 09:08:54 +0000

    *node tank

--- EgretProjects/xb.tank/nodejs/index.js
+++ EgretProjects/xb.tank/nodejs/index.js
@@ -20,24 +20,17 @@ io.on('connection', function(socket){
     socket.on('login', function(data,callback){
     	var userType = data.userType;
         var rid = data.rid;
-		//var openid = data.openid;
-       // console.log("----" + "用户加入" + "rid:" + rid + " userType:" + userType + " openid:" + openid);
+		var openid = data.openid;
+        console.log("----" + "用户加入" + "rid:" + rid + " userType:" + userType + " openid:" + openid);
         //将新加入用户的唯一标识当作socket的名称，后面退出的时候会用到
         socket.rid = rid;
-		//socket.openid = openid;
-
-        //检查在线列表，如果不在里面就加入
-    	if(userType == "pc" && !roomList[rid]){
-    		initPC(socket);
-    		pcUser[rid] = socket;
-    		console.log('PC用户加入');
-    	}else if(userType == "mobile"){
-    		initMobile(socket);
-			if(mobileUser[rid] == undefined){
-				mobileUser[rid] = {};
-			}
-    		mobileUser[rid][data.openid] = socket;
-    		console.log('手机用户加入');
+		socket.openid = openid;
+        //pc用户加入
+    	if(userType == "pc"){
+    		pcUserJoin(rid, socket);
+    	}
+    	if(userType == "mobile"){
+    		mobileUserJoin(rid, socket);
     	}
         //回调函数
         callback({success:true});
@@ -49,8 +42,6 @@ io.on('connection', function(socket){
         		pcSocket.emit('userJoin', {openid:openid,headimgurl:"resource/assets/home/home_p1.png",nickname:"ABC"});
         	}
         }
-
-        
     });
 
      
@@ -73,17 +64,33 @@ io.on('connection', function(socket){
    
 });
 
-function initPC(socket){
+/**pc用户加入*/
+function pcUserJoin(rid, socket){
+	//房间已存在
+	if(roomList[rid] != null){
+		socket.emit('login', {'success':false, 'msg':'房间已存在'});
+		return;
+	}
+	//新建房间
+	roomList[rid] = {};
+	roomList[rid].pc = socket;
+	listenerSocketPC(socket);
+	console.log('PC用户加入');
+}
+
+/**监听pc socket*/
+function listenerSocketPC(socket){
 	//更新rid
     socket.on('upRid',function(data){
 		console.log("upRid:",data.rid);
-		
-		//保存pc端
+		//清理房间
+		delete roomList[socket.id];
 		delete roomList[data.rid];
+		//新建房间
 		roomList[data.rid] = {};
 		roomList[data.rid].pc = socket;
 		
-		/*
+		//发送排行榜测试数据
 		var data = {
 			"heroRankList":[
 				{"p1HeadUrl":"resource/assets/home/home_p1.png", "p2HeadUrl":"resource/assets/home/home_p1.png", stage:3,wave:3},
@@ -95,45 +102,68 @@ function initPC(socket){
 				],
 			"historyScore":999999
 		}
-		*/
-		var data = {};
 		socket.emit("rank", data);
     });
 
 	//开始游戏
     socket.on('startGame',function(data){
-    	if(pcUser[socket.rid]){
-			if(mobileUser[socket.rid]){
-				var mobileObj = mobileUser[socket.rid];
-				for(var key in mobileObj) {
-					 mobileObj[key].emit('startGame');
-				}
+    	if(roomList[socket.rid].pc && roomList[socket.rid].mobile){
+			var mobileObj = roomList[socket.rid].mobile;
+			for(var key in mobileObj) {
+				 mobileObj[key].emit('startGame');
 			}
     	}
     });
 
 	//游戏结束
     socket.on('gameOver',function(data,callback){
-		console.log("-------rev gameOver rid=" + socket.rid);
-    	if(pcUser[socket.rid]){
-			if(mobileUser[socket.rid]){
-				var mobileObj = mobileUser[socket.rid];
-				for(var key in mobileObj) {
-					 mobileObj[key].emit('gameOver',{"historyScore":9999,"scoreRank":3,"p1KillRank":5,"p2KillRank":6});
-				}
-			}	
+    	if(roomList[socket.rid].pc && roomList[socket.rid].mobile){
+			var mobileObj = roomList[socket.rid].mobile;
+			for(var key in mobileObj) {
+				 mobileObj[key].emit('gameOver',{"historyScore":9999,"scoreRank":3,"p1KillRank":5,"p2KillRank":6});
+			}
     	}
-		console.log("callback gameOver to PC");
 		callback({"historyScore":9999,"scoreRank":3,"p1KillRank":5,"p2KillRank":6});
     });
 }
 
-function initMobile(socket){
+/**手机用户加入*/
+function mobileUserJoin(rid, socket){
+	//检查房间是否存在
+	if(roomList[rid] == null){
+		socket.emit('login', {'success':false, 'msg':'房间不存在'});
+		return;
+	}
+	if(roomList[rid].mobile == null){
+		roomList[rid].mobile = {};
+	}
+	//检查人数是否足够
+	if(getObjLength(roomList[rid].mobile) >= 2){
+		socket.emit('login', {'success':false, 'msg':'人数已满'});
+		return;
+	}
+
+	roomList[rid].mobile[data.openid] = socket;
+	listenerSocketMobile(socket);
+	console.log('手机用户加入');
+}
+
+/**监听手机端 socket*/
+function listenerSocketMobile(socket){
 	//接收来自手机的消息
     socket.on('action',function(data){
 		var actionType = data.actionType;
-		if(pcUser[socket.rid]){
-			pcUser[socket.rid].emit('action',{actionType:actionType,openid:socket.openid});
+		if(roomList[socket.rid]){
+			roomList[socket.rid].pc.emit('action',{actionType:actionType,openid:socket.openid});
 		}
     });
+}
+
+/**获取obj长度*/
+function getObjLength(obj){
+	var count = 0;
+	for(var key in obj){
+		count++;
+	}
+	return count;
 }
