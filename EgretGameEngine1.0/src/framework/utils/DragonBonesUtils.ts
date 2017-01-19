7be0b9002ab798d4d4cc32c45ab8e4727ac74625
gamedaybyday <gamedaybyday@30016ba7-4c49-4331-af59-b1367cd06790>
2017-01-19 08:53:06 +0000

    *框架修改

---
+++ EgretGameEngine1.0/src/framework/utils/DragonBonesUtils.ts
@@ -0,0 +1,47 @@
+/**
+ * 骨骼动画工具
+ * @author chenkai 
+ * @date 2017/1/19
+ */
+class DragonBonesUtils extends SingleClass{
+    /**工厂*/
+    private factory: dragonBones.EgretFactory;
+    
+	public constructor() {
+    	super();
+        this.factory = new dragonBones.EgretFactory();
+	}
+	
+	/**
+	 * 添加龙骨动画数据
+	 * @dragonData 龙骨数据
+	 * @texureJson 贴图集数据
+	 * @texture 贴图
+	 */ 
+    public addDragonBones(dragonbonesData:any, textureData:any, texture:egret.Texture){
+        this.factory.addDragonBonesData(dragonBones.DataParser.parseDragonBonesData(dragonbonesData));
+        this.factory.addTextureAtlas(new dragonBones.EgretTextureAtlas(texture,textureData));
+	}
+	
+	/**
+	 * 获取骨架
+	 * @armatureName 骨架名
+	 * @return 返回骨架
+	 */ 
+	public getArmatureDisplay(armatureName:string):dragonBones.EgretArmatureDisplay{
+        return this.factory.buildArmatureDisplay(armatureName);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
