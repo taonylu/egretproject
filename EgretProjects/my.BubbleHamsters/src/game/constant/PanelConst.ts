4887496a8c22f6fe8c8c8924d8aba04ef65e293d
gamedaybyday <gamedaybyday@30016ba7-4c49-4331-af59-b1367cd06790>
2017-02-07 09:31:35 +0000

    -删除泡泡龙项目

--- EgretProjects/my.BubbleHamsters/src/game/constant/PanelConst.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 弹框常量
- * @author chenkai 
- * @date 2016/12/28
- */
-class PanelConst {
-	
-}
--- EgretProjects/my.BubbleHamsters/src/game/constant/SceneConst.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 场景常量
- * @author chenkai 
- * @date 2016/12/28
- */
-class SceneConst {
-	
-}
