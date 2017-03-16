a5803a24c6bca05ddc2aa582a0febc029026c3be
gamedaybyday <gamedaybyday@30016ba7-4c49-4331-af59-b1367cd06790>
2017-03-16 03:20:42 +0000

    *将框架移动到git

--- EgretGameEngine1.0/src/game/constant/CmdConst.ts
+++
@@ -1,9 +0,0 @@
-/**
- * command常量
- * @author chenkai
- * @since 2017/2/8
- */
-class CmdConst {
-    /**启动命令*/
-	public static STARTUP:string = "STARTUP";
-}
--- EgretGameEngine1.0/src/game/constant/PanelConst.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 弹框常量
- * @author chenkai 
- * @since 2016/12/28
- */
-class PanelConst {
-	
-}
